4785ca017b9c009d3d7881440f8e8e184ea17267
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-01-28 10:41:01 +0900

    First commit

---
+++ MyTest/MyTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyTestApp.swift
+//  MyTest
+//
+//  Created by Osamu Takiya on 2024/01/28.
+//
+
+import SwiftUI
+
+@main // アプリのエントリポイントを示す
+struct MyTestApp: App {
+    var body: some Scene {
+        // アプリが起動したときに表示される画面
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
