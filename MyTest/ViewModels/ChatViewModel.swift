190eb05d8b6aeacd87745cbd3a6a9b7646e1473e
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-01-28 14:40:37 +0900

    feat: 🎸 JSON データをデコードする処理を作成した

--- MyTest/ViewModels/ChatViewModel.swift
+++ MyTest/ViewModels/ChatViewModel.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class ChatViewModel {
-    
+    private func fetchChatDate() -> [Chat] {
+        let fileName = "chatData.json"
+        let data: Data
+
+        guard let filePath = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+            fatalError("\(fileName)が見つかりませんでした。")
+        }
+
+        do {
+            // この時点ではまだ生のテキストデータ
+            data = try Data(contentsOf: filePath)
+        } catch {
+            fatalError("\(fileName)のロードに失敗しました。")
+        }
+
+        do {
+            // TODO: Decodable プロトコルに準拠させる（？）
+            // TODO: [Chat].self とは？
+            return try JSONDecoder().decode([Chat].self, from: data)
+        } catch {
+            fatalError("\(fileName)を\(Chat.self)に変換することに失敗しました。")
+
+        }
+    }
 }
