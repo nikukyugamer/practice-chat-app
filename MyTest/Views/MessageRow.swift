9bf21f527a2897ea5ff4dbaf8aef96f65dd0220a
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-01-28 11:39:03 +0900

    feat: 🎸 MessageArea の UI を作成した

--- MyTest/Views/MessageRow.swift
+++ MyTest/Views/MessageRow.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack {
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top) {
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 48, height: 48)
+            Text("こんにちは！")
+                .padding()
+                .background(.white)
+                .cornerRadius(30)
+            VStack(alignment: .trailing) {
+                Spacer()
+                Text("既読")
+                Text(formattedDateString)
+            }
+            .foregroundColor(.secondary)
+            .font(.footnote)
+            Spacer()
         }
         .padding(.bottom)
+    }
+
+    private var formattedDateString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+
+        return formatter.string(from: Date())
     }
 }
 
 #Preview {
     MessageRow()
+        .background(.cyan)
 }
