7e202a5c30685f6391637ac4f19516a37701d5e4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2024-01-28 18:43:32 +0900

    feat: 🎸 List のナビゲーションに名前を表示することにした

--- MyTest/ViewModels/ChatViewModel.swift
+++ MyTest/ViewModels/ChatViewModel.swift
@@ -65,4 +65,23 @@ class ChatViewModel: ObservableObject {
 //        messages.append(newMessage)
         chatData[index].messages.append(newMessage)
     }
+
+    func getTitle(messages: [Message]) -> String {
+        var title = ""
+        var userIds: [String] = []
+
+        for message in messages {
+            let id = message.user.id
+
+            if id == User.currentUser.id { continue }
+            if userIds.contains(id) { continue }
+
+            userIds.append(id)
+
+            let name = message.user.name
+            title += title.isEmpty ? "\(name)" : ", \(name)"
+        }
+
+        return title
+    }
 }
